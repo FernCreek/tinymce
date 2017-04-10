a2d511a3373e4ab080fd67476cdae99601226309
bradleyke <bradleyk@seapine.com>
2017-04-10 11:48:55 -0400

    Updated bolt config for nativemodern theme

--- tools/bolt/config/browser.js
+++ tools/bolt/config/browser.js
@@ -4,6 +4,7 @@ configure({
         '../../../js/tinymce/plugins/media/config/bolt/browser.js',
         '../../../js/tinymce/plugins/lists/config/bolt/browser.js',
         '../../../js/tinymce/themes/inlite/config/bolt/browser.js',
-        '../../../js/tinymce/themes/modern/config/bolt/browser.js'
+        '../../../js/tinymce/themes/modern/config/bolt/browser.js',
+        '../../../js/tinymce/themes/nativemodern/config/bolt/browser.js',
     ]
 });
