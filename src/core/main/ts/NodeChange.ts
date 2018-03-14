36bbc1729ed54cd339618aa9195ac66af2909b1a
bradleyke <bradleyk@seapine.com>
2018-03-14 09:12:24 -0400

    Reapplying previous patch to new TinyMCE structure
    See:
    https://github.com/FernCreek/tinymce/commit/73bac7448ba267a7c1dc92f5095d2d146092c5f3#diff-7d67d31ce0e5dfe4d8a78f1a9ca3df7e

--- src/core/main/ts/NodeChange.ts
+++ src/core/main/ts/NodeChange.ts
@@ -102,13 +102,9 @@ export default function (editor) {
     if (!e.isDefaultPrevented()) {
       // Delay nodeChanged call for WebKit edge case issue where the range
       // isn't updated until after you click outside a selected image
-      if (editor.selection.getNode().nodeName === 'IMG') {
-        Delay.setEditorTimeout(editor, function () {
-          editor.nodeChanged();
-        });
-      } else {
+      Delay.setEditorTimeout(editor, function () {
         editor.nodeChanged();
-      }
+      });
     }
   });
 
