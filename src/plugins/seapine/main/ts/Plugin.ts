cdf144694663be05ed87e59aedb1fc7637a76e55
bradleyke <bradleyk@seapine.com>
2018-03-21 12:53:45 -0400

    Moved file comment block

--- src/plugins/seapine/main/ts/Plugin.ts
+++ src/plugins/seapine/main/ts/Plugin.ts
@@ -1,14 +1,15 @@
-import PluginManager from 'tinymce/core/api/PluginManager';
-import Formats from './api/Formats';
-import Commands from './api/Commands';
-import FontUtils from './core/FontUtils';
-
 /**
  * @file Plugin.ts
  * @copyright 2016-2018 Perforce Software, Inc. and its subsidiaries.
  * Released under LGPL License.
  * License: http://www.tinymce.com/license
  */
+
+import PluginManager from 'tinymce/core/api/PluginManager';
+import Formats from './api/Formats';
+import Commands from './api/Commands';
+import FontUtils from './core/FontUtils';
+
 PluginManager.add('seapine', function (editor) {
   Formats.register(editor);
   Commands.register(editor);
