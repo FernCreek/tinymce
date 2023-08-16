90a761541cd116743ff66320c1a0e72fd68c7e96
Jonathan Alley <jalley@perforce.com>
2023-08-16 14:26:36 -0400

    Reapply patch 5518df1

--- modules/tinymce/src/core/main/ts/mode/Readonly.ts
+++ modules/tinymce/src/core/main/ts/mode/Readonly.ts
@@ -5,11 +5,10 @@
  * For commercial licenses see https://www.tiny.cloud/
  */
 
-import { Arr, Optional, Strings } from '@ephox/katamari';
+import { Arr, Optional } from '@ephox/katamari';
 import { Attribute, Class, Compare, SelectorFilter, SelectorFind, SugarElement } from '@ephox/sugar';
 
 import Editor from '../api/Editor';
-import VK from '../api/util/VK';
 import * as EditorFocus from '../focus/EditorFocus';
 
 const internalContentEditableAttr = 'data-mce-contenteditable';
@@ -121,8 +120,6 @@ const registerReadOnlyContentFilters = (editor: Editor) => {
   }
 };
 
-const isClickEvent = (e: Event): e is MouseEvent => e.type === 'click';
-
 /*
 * This function is exported for unit testing purposes only
 */
@@ -131,28 +128,8 @@ const getAnchorHrefOpt = (editor: Editor, elm: SugarElement): Optional<string> =
   return SelectorFind.closest<HTMLAnchorElement>(elm, 'a', isRoot).bind((a) => Attribute.getOpt(a, 'href'));
 };
 
-const processReadonlyEvents = (editor: Editor, e: Event) => {
-  /*
-    If an event is a click event on or within an anchor, and the CMD/CTRL key is
-    not held, then we want to prevent default behaviour and either:
-      a) scroll to the relevant bookmark
-      b) open the link using default browser behaviour
-  */
-  if (isClickEvent(e) && !VK.metaKeyPressed(e)) {
-    const elm = SugarElement.fromDom(e.target as Node);
-    getAnchorHrefOpt(editor, elm).each((href) => {
-      e.preventDefault();
-      if (/^#/.test(href)) {
-        const targetEl = editor.dom.select(`${href},[name="${Strings.removeLeading(href, '#')}"]`);
-        if (targetEl.length) {
-          editor.selection.scrollIntoView(targetEl[0], true);
-        }
-      } else {
-        window.open(href, '_blank', 'rel=noopener noreferrer,menubar=yes,toolbar=yes,location=yes,status=yes,resizable=yes,scrollbars=yes');
-      }
-    });
-  }
-};
+// We allow clicking links in readonly mode
+const processReadonlyEvents = (_editor: Editor, _e: Event) => {};
 
 const registerReadOnlySelectionBlockers = (editor: Editor) => {
   editor.on('ShowCaret', (e) => {
